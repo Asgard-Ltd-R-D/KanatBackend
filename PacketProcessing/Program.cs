72e1f6e9139c9c2808fd5daa40a45f02980468d2
ShovalShabiAsgard <shoval.s@asgardsys.co.il>
2025-08-21 17:33:02 +0300

    Applied web application start-up class

--- PacketProcessing/Program.cs
+++ PacketProcessing/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Microsoft.AspNetCore.Builder;
+using PacketProcessing.Config;
+
+var builder = WebApplication.CreateBuilder(args);
+
+/// <summary>
+/// Configure all application services and dependencies
+/// </summary>
+ConfigurationInjection.InjectConfigurations(builder);
+
+var app = builder.Build();
+
+/// <summary>
+/// Ensure database is up to date with latest migrations
+/// </summary>
+DatabaseMigrationHelper.EnsureDatabaseUpToDate(app);
+
+/// <summary>
+/// Configure all middleware components
+/// </summary>
+ConfigurationInjection.InjectMiddleware(app);
+
+/// <summary>
+/// Start the web application
+/// </summary>
+app.Run();

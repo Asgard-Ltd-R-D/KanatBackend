ca5212ce0704624a99ca75f5a26e45b12bbfc548
TamirAsgard <Tamir.s@asgardsys.co.il>
2025-08-21 16:38:25 +0300

    entities

---
+++ PacketProcessing/Entities/MotionPacketEntity.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PacketProcessing.Entities;
+
+public class MotionPacketEntity
+{
+    [Key]
+    public Guid Id { get; set; }
+    public bool Type { get; set; }
+    public string OpCode { get; set; }
+    public string OpCodeDescription { get; set; }
+    public int Axis { get; set; }
+    public float? FloatValue { get; set; }
+    public ulong Timestamp { get; set; }
+}
---
+++ PacketProcessing/Entities/OnVIFPacketEntity.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PacketProcessing.Entities;
+
+public class OnVIFPacketEntity
+{
+    [Key]
+    public Guid Id { get; set; }
+    public bool Type { get; set; }
+    public string Description { get; set; }
+    public float? Zoom { get; set; }
+    public float Measurement { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public ulong Timestamp { get; set; }
+}

3bd2a82b44eeb45b4aa28e1c0fe49dfc580b8b29
ShovalShabiAsgard <shoval.s@asgardsys.co.il>
2025-09-30 11:45:00 +0300

    Wrote utility for the BPF filter string

---
+++ PacketProcessing/src/Utils/Filters/BpfFilterBuilder.cs
@@ -0,0 +1,36 @@
+namespace PacketProcessing.Utils.Filters;
+
+public static class BpfFilterBuilder
+{
+    /// <summary>
+    /// Builds a BPF filter string based on the protocol and IPs.
+    /// returns the filter constructed as {protocol} and (host {ip} or host {ip} or ...)
+    /// </summary>
+    /// <param name="protocol"></param>
+    /// <param name="ips"></param>
+    /// <returns></returns>
+    public static string Build(string protocol, IEnumerable<string>? ips)
+    {
+        // Base: protocol
+        var proto = protocol?.ToLowerInvariant() switch
+        {
+            "tcp" => "tcp",
+            "udp" => "udp",
+            "http" => "tcp port 80 or tcp port 443", // basic http(s)
+            "any" => string.Empty,
+            _ => protocol ?? string.Empty
+        };
+
+        var conditions = new List<string>();
+        if (!string.IsNullOrWhiteSpace(proto))
+            conditions.Add(proto);
+
+        if (ips is not null && ips.Any())
+        {
+            var ipExprs = ips.Select(ip => $"host {ip}");
+            conditions.Add($"({string.Join(" or ", ipExprs)})");
+        }
+
+        return string.Join(" and ", conditions);
+    }
+}

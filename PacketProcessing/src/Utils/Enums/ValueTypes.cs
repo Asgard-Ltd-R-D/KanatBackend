feb7fdafdfd67f18292082db023f3161261e5f63
ShovalShabiAsgard <shoval.s@asgardsys.co.il>
2025-10-19 13:41:59 +0300

    Fixed value members to be positional and not by value

--- PacketProcessing/src/Utils/Enums/ValueTypes.cs
+++ PacketProcessing/src/Utils/Enums/ValueTypes.cs
@@ -5,9 +5,9 @@ namespace PacketProcessing.Utils.Enums;
 /// </summary>
 public enum ValueTypes
 {
-    None = 0,             // 0 bits
-    UInt8 = 8,            // 1 byte
-    UInt16BE = 16,        // 2 bytes big-endian
-    UInt32BE = 32,        // 4 bytes big-endian
-    Float32BE = 32,       // 4 bytes IEEE754 big-endian
+    None,             // 0 bits
+    UInt8,            // 1 byte
+    UInt16BE,        // 2 bytes big-endian
+    UInt32BE,        // 4 bytes big-endian
+    Float32BE,       // 4 bytes IEEE754 big-endian
 }

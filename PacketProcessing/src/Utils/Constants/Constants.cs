5cfce6178e04fce5d6373380b565308c803fcc7f
ShovalShabiAsgard <shoval.s@asgardsys.co.il>
2025-10-29 10:02:06 +0200

    Added necessary constants

--- PacketProcessing/src/Utils/Constants/Constants.cs
+++ PacketProcessing/src/Utils/Constants/Constants.cs
@@ -5,16 +5,23 @@ public static class Constants {
     public const string SIGNALR_ACK = "Ack";
 
     // ONVIF constants
-    public const string ONVIF_REPORT_IP="132.8.7.121";
+    public const ushort ONVIF_REPORT_TCP_PORT=8080;
     public const string ONVIF_XML_DAY="day";
     public const string ONVIF_XML_NIGHT="night_combined";
     public const string ONVIF_XML_LRF="laser_range_finder";
+
+    // Motion packet constants
+    public const ushort MOTION_REPORT_PORT=4949;
 
     // OnVIF packet descriptions
     public const string ONVIF_FOV_REQ="FOV_REQ";
     public const string ONVIF_FOV_STS="FOV_STS";
     public const string ONVIF_LRF_REQ="LRF_REQ";
     public const string ONVIF_LRF_STS="LRF_STS";
+
+    // Safety packet constants
+    public const string PBE_IP="132.8.7.101";
+    public const string SBE_IP="132.8.7.102";
 
     // Data pipes names
     public const string MOTION_PACKETS_TAG = "motion_packets";
